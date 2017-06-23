92233e38cf229637835b0f528ba37cb9d2f66150
kirchick <kir.vlasuck@yandex.ru>
2017-06-24 00:32:26 +0400

    added list traners. delete traners,delete card orders

---
+++ FitnessCentreSite/Models/TranerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FitnessCentreSite.Models
+{
+    public class TranerViewModel
+    {
+        [Required]
+        [Display(Name = "Имя тренера")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+    }
+}
